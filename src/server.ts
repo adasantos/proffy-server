1a7a9a64c7312ff06de4ea2e50ad719f05492a5c
Andre Dionisio <andredionisio12@gmail.com>
2021-01-05 03:45:31 -0300

    implementing routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-app.get('/users', (request, response) => {
-  return response.json({ message: 'Hello World'});
-})
+app.use(routes);
 
 app.listen(3333);
